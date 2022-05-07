e05817b114f945072ed6bb639b2449d18b9043f4
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-05-06 23:21:42 -0700

    api created

--- index.js
+++ index.js
@@ -3,10 +3,49 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const {
+  MongoClient,
+  ServerApiVersion,
+  MongoRuntimeError,
+  ObjectId,
+} = require("mongodb");
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zfiue.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client
+      .db("warehouseManagement")
+      .collection("product");
+
+    //getting all products
+    //http://localhost:5000/product
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    //getting one product by id
+    app.get("/product/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hello world!");
