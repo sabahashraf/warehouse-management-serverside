40c4fe614f60837e88bd711c76ebd27c8a66c177
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-05-09 06:20:21 -0700

    final

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function verifyJWT(req, res, next) {
     if (err) {
       return res.status(403).send({ message: "Forbidden access" });
     }
-    console.log("decoded", decoded);
+
     req.decoded = decoded;
     next();
   });
