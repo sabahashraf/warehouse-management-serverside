f86bc06348b3e2fd69c74e49746795d1c7cb4e1e
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-05-09 00:12:27 -0700

    delete route added

--- index.js
+++ index.js
@@ -42,15 +42,49 @@ async function run() {
       const product = await productCollection.findOne(query);
       res.send(product);
     });
+    //geting items
+    app.get("/item", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = productCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
     //posting one product
     app.post("/product", async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
       res.send(result);
     });
+    //updating one product
+    app.put("/product/:inventoryId", async (req, res) => {
+      const { inventoryId } = req.params;
+      console.log(inventoryId);
+      const updatedProduct = req.body;
+      console.log(updatedProduct);
+
+      const filter = { _id: ObjectId(inventoryId) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: { quantity: updatedProduct.quantity },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
 
     //deleting one product by id
     app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+    //deleting one item by id
+    app.delete("/item/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await productCollection.deleteOne(query);
